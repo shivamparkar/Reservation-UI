e2db92cf8834737cf354982463ca9571cc622dbe
Shivam <parkarshivam@gmail.com>
2025-04-09 17:05:01 +0530

    pushing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/List/List";
+import Hotel from "./pages/hotel/Hotel";
+
 function App() {
-  return <div>Hello World!</div>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/hotels" element={<List />} />
+    <Route path="/hotels/:id" element={<Hotel />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
